1ba102583130e9f86ff39318b337e7c12cc71a9f
anh.le <anh.le@bstarsolutions.com>
2023-08-07 00:40:36 +0700

    Correct import paths

--- go-temporal-example-01/cmd/worker/main.go
+++ go-temporal-example-01/cmd/worker/main.go
@@ -6,24 +6,27 @@ import (
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 
-	"money-transfer-project-template-go/app"
+	"github.com/captain-corgi/go-temporal-example-01/internal/momo/activity"
+	"github.com/captain-corgi/go-temporal-example-01/internal/momo/model"
+	"github.com/captain-corgi/go-temporal-example-01/internal/momo/workflow"
 )
 
 func main() {
-
+	// Create the client object just once per process
 	c, err := client.Dial(client.Options{})
 	if err != nil {
 		log.Fatalln("Unable to create Temporal client.", err)
 	}
 	defer c.Close()
 
-	w := worker.New(c, app.MoneyTransferTaskQueueName, worker.Options{})
+	// Create worker
+	w := worker.New(c, model.MoneyTransferTaskQueueName, worker.Options{})
 
 	// This worker hosts both Workflow and Activity functions.
-	w.RegisterWorkflow(app.MoneyTransfer)
-	w.RegisterActivity(app.Withdraw)
-	w.RegisterActivity(app.Deposit)
-	w.RegisterActivity(app.Refund)
+	w.RegisterWorkflow(workflow.MoneyTransfer)
+	w.RegisterActivity(activity.Withdraw)
+	w.RegisterActivity(activity.Deposit)
+	w.RegisterActivity(activity.Refund)
 
 	// Start listening to the Task Queue.
 	err = w.Run(worker.InterruptCh())
